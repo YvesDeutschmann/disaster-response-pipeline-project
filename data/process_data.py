b08a22856aca3b5b3c58b55fcba973ad0c56878f
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-07-08 19:57:44 +0200

    Added data processing

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,52 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Loads the messages and categories from flatfiles.
+    Merges both datasets into a DataFrame.
+    """
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories)
 
+    return df
 
 def clean_data(df):
-    pass
+    """Clean up the message dataframe."""
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';', expand=True)
+
+    # extract column names for categories from first row
+    row = categories.iloc[0,:]
+    colnames = [column[:-2] for column in row.values]
+    categories.columns = colnames
+
+    # Convert category values to 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # Replace categories column in df with new category columns
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(r'sqlite:///{}'.format(database_filename))
+    df.to_sql('CleanData', engine, index=False, if_exists='replace')  
 
 
 def main():
